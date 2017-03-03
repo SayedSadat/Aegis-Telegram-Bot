c787b11791032a59a70dbe41a829b86184642dca
Vir <mrvir@protonmail.com>
2017-03-03 13:26:40 +0700

    fix exception when send wrong link

--- modules/mp3.py
+++ modules/mp3.py
@@ -9,8 +9,8 @@ def process(bot, update):
     else:
         parameter = update.message.text.replace("/mp3 ", "")
         url_valid = re.match("(https?:\/\/)?mp3\.zing\.vn\/bai-hat\/[\w\d\-]+/([\w\d]{8})\.html", parameter)
-        string_valid = re.search("([A-Z0-9]{8})", parameter).group(1)
         if url_valid:
+            string_valid = re.search("([A-Z0-9]{8})", parameter).group(1)
             content = requests.get('http://phulieuminhkhang.com/images/Sanpham/api.php?parameter='+string_valid).text
             decoded = json.loads(content)
             update.message.reply_text("*Song:* `"+decoded['title']+"`, *Artist:* `"+decoded['artist']+"`\n[128Kbps]("+decoded['link_download']['128']+")\n[320Kbps]("+decoded['source']['320']+")", 'Markdown')
